41695b8c35c8c01ce1305209c8edcaa5ef06d844
Mohamed Esmail <moh12345678912mmo@gmail.com>
2025-12-10 04:14:43 +0000

    add

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const backgrounds = [
     'https://images.squarespace-cdn.com/content/v1/5fe4caeadae61a2f19719512/fbabc357-b945-4e72-8881-b94e68522174/Cozy+Home+Office?format=2500w',
     'https://storage.ko-fi.com/cdn/useruploads/display/f9d344fe-3715-41b8-8996-6d844cac422b_scrivaniapreview.gif',
     'http://i.imgur.com/MDYkP0A.gif',
-    'https://i.pinimg.com/originals/0b/1d/fc/0b1dfc8ebe6f365f4208cb0cff5c46c9.gif',
+    'https://i.pinimg.com/originals/9f/42/2b/9f422b3823bc356e0e801f2504452c4c.gif',
     'https://i.pinimg.com/originals/1c/98/33/1c983310d3e6765916c63294e4b99de4.gif',
     'cozy-home-interior-anime-style.jpg',
     'wp11702872-cozy-lofi-wallpapers.jpg'
